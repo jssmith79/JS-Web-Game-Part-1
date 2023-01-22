02e183578181ee2d899f4536511b89c2e6442b2f
jssmith79 <i.am.hef@gmail.com>
2023-01-22 11:43:53 -0800

    class updates

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage()
+{
+    let greenCharacter = document.createElement('img')
+    greenCharacter.src = 'assets/green-character.gif'
+    greenCharacter.style.position = 'fixed'
+    greenCharacter.style.left = '100px'
+    greenCharacter.style.bottom = '100px'
+    document.body.append(greenCharacter)
+
+    let pineTree = document.createElement('img')
+    pineTree.src = 'assets/pine-tree.png'
+    pineTree.style.position = 'fixed'
+    pineTree.style.left = '450px'
+    pineTree.style.bottom = '200px'
+    document.body.append(pineTree)
+        
+
+    let sword = document.createElement('img')
+    sword.src = 'assets/sword.png'
+    sword.position = 'fixed'
+    sword.left = '500px'
+    sword.bottom = '405px'
+    document.body.append(sword)
+    sword.addEventListener('click', function(){
+    sword.remove()})
+ 
+    let tree = document.createElement('img')
+    tree.src = 'assets/tree.png'
+    tree.style.position = 'fixed'
+    tree.style.left = '200px'
+    tree.style.bottom = '300px'
+    document.body.append(tree)
+    
+    let pillar = document.createElement('img')
+    pillar.src = 'assets/pillar.png'
+    pillar.style.position = 'fixed'
+    pillar.style.left = '350px'
+    pillar.style.bottom = '100px'
+    document.body.append(pillar)
+}
+
+newImage()
